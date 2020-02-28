369400397358a202c49b429df33c650c3e5accfe
zlopez101 <zachlopez9@gmail.com>
2020-02-27 23:47:23 -0600

    fixed interested_in functionality

--- TxChange/routes.py
+++ TxChange/routes.py
@@ -62,8 +62,14 @@ def logout():
 @app.route("/profile", methods=["GET", "POST"])
 @login_required
 def profile():
-	t_sell = Ticket.query.filter_by(owner=current_user).all()
-	return render_template("profile.html", title="Profile Page", tickets=t_sell)
+    t_sell = Ticket.query.filter_by(owner=current_user).all()
+    t_interest = User.query.filter_by(id=current_user).first().intereste_in
+    return render_template(
+        "profile.html",
+        title="Profile Page",
+        tickets=t_sell,
+        interestingtickets=t_interest,
+    )
 
 
 @app.route("/NewTicket", methods=["GET", "POST"])
@@ -76,7 +82,7 @@ def new_ticket():
             venue=form.venue.data,
             price=form.price.data,
             concert_date_time=form.concert_date_time.data,
-						owner=current_user
+            owner=current_user,
         )
         db.session.add(ticket)
         db.session.commit()
@@ -87,54 +93,41 @@ def new_ticket():
         return redirect(url_for("home"))
     return render_template("create_ticket.html", title="Post a New Ticket", form=form)
 
+
 @app.route("/ticket/<ticket_id>/bid", methods=["GET", "POST"])
 @login_required
 def ticket(ticket_id):
-	form = Test()
-	ticket = Ticket.query.get_or_404(ticket_id)
-	if form.validate_on_submit():
-		if form.amount.data <= ticket.current_best_bid:
-			flash("Not high enough!", "danger")
-			return redirect(url_for('discover'))
-		ticket.current_best_bid = form.amount.data
-		ticket.current_best_bidder = current_user
-		db.session.add(ticket)
-		db.session.commit()
-		flash("Bid Entered", "success")
-		return redirect(url_for("home"))
-	return render_template('ticket.html', title="Bid", form=form, ticket=ticket)
+    form = Test()
+    ticket = Ticket.query.get_or_404(ticket_id)
+    if form.validate_on_submit():
+        if form.amount.data <= ticket.current_best_bid:
+            flash("Not high enough!", "danger")
+            return redirect(url_for("discover"))
+        ticket.current_best_bid = form.amount.data
+        ticket.current_best_bidder = current_user
+        db.session.add(ticket)
+        db.session.commit()
+        flash("Bid Entered", "success")
+        return redirect(url_for("home"))
+    return render_template("ticket.html", title="Bid", form=form, ticket=ticket)
 
 
-@app.route('/interestedin/<ticket_id>/', methods=["GET, POST"])
+@app.route("/interestedin/<ticket_id>/", methods=["GET", "POST"])
 @login_required
 def interested(ticket_id):
-	ticket = Ticket.query.get_or_404(ticket_id)
-	ticket.user_interested.append(current_user)
-	db.session.add(ticket)
-	db.session.commit()
-	flash(f"You saved ticket for {ticket.artist} at {ticket.venue}.", "success")
-	return redirect(url_for('profile'))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    ticket = Ticket.query.get_or_404(ticket_id)
+    ticket.users_interested.append(current_user)
+    db.session.add(ticket)
+    db.session.commit()
+    flash(f"You saved ticket for {ticket.artist} at {ticket.venue}.", "success")
+    return redirect(url_for("profile"))
 
 
 @app.route("/test", methods=["GET", "POST"])
 def test():
-	form = Ticketbid()
-	if form.validate_on_submit():
-		flash('test worked')
-		return redirect(url_for("home"))
-	return render_template('test.html', title="test", form=form)
+    form = Ticketbid()
+    if form.validate_on_submit():
+        flash("test worked")
+        return redirect(url_for("home"))
+    return render_template("test.html", title="test", form=form)
 
